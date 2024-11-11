ea84bd8c8f530437756da21edfa8f175004cedad
Your Name <tamndph41864@fpt.edu.vnvn>
2024-11-11 23:24:58 +0700

    add new interface Service

---
+++ laravel/app/Services/Interfaces/SearchHistory/SearchHistoryServiceInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services\Interfaces\SearchHistory;
+
+interface SearchHistoryServiceInterface
+{
+    public function paginate();
+
+    public function create();
+
+    public function update($id);
+
+    public function destroy($id);
+
+    public function getAll();
+}
