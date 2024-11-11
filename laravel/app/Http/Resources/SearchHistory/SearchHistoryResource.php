467dac4b59ab16595d9a65c289af1667bd645e95
Your Name <tamndph41864@fpt.edu.vnvn>
2024-11-11 23:45:47 +0700

    working resource

---
+++ laravel/app/Http/Resources/SearchHistory/SearchHistoryResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources\SearchHistory;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SearchHistoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // {
+        //     "width": "4",
+        //     "effect": "fade",
+        //     "height": "4",
+        //     "autoPlay": "true",
+        //     "showArrow": "true",
+        //     "navigation": "thumbnails",
+        //     "effectSpeed": "4",
+        //     "pauseOnHover": "true",
+        //     "transitionSpeed": "4"
+        //   }
+        return [
+            'id'        => $this->id,
+            'key'       => $this->id,
+            'keyword'   => $this->keyword,
+            'count'     => $this->count,
+        ];
+    }
+}
