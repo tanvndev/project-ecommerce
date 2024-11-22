3cde5d174cbadaa3e99137f6cff4308975511047
TanVu <tanvnph33606@gmail.com>
2024-11-22 16:50:00 +0700

    Change favicon

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -4,10 +4,9 @@ import '~/assets/css/main.min.css'
 useSeoMeta({
   title: 'Trang chủ',
   ogTitle: 'Trang chủ',
-  description: 'This is my amazing site, let me tell you all about it.',
-  ogDescription: 'This is my amazing site, let me tell you all about it.',
-  ogImage: 'https://example.com/image.png',
-  twitterCard: 'summary_large_image',
+  description: 'Trang chủ.',
+  ogDescription: 'Trang chủ.',
+  ogImage: '/og-image.png'
 })
 
 const widgetCodes = ref([])
